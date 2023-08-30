9347c2f25a82c04e7c833df0c6ad48c674668b8a
SimbaFs <simba.fs@gmail.com>
2023-08-30 21:46:32 +0800

    add commander

--- index.js
+++ index.js
@@ -1,57 +1,21 @@
 #!/bin/env node
 
-const path = require('path')
-const matter = require('gray-matter')
-const fs = require('fs')
+const { program } = require('commander')
 const { getOgFromMd } = require('./src/getOg')
 
-let inFile = ''
-let outFile = ''
-let author = 'SimbaFs'
-let font
-let template
+program
+    .argument('<input>', 'the input markdown file')
+    .argument('[output]', 'the output png file, default og/${input}')
+    .option('--author <name>')
+    .option('--font <path>')
+    .option('--template <path>')
+    .action((input, output = `og/${input}`.replace(/\.md$/, '.png'), opts) => {
+        console.log({ input, output, opts })
+        getOgFromMd({
+            ...opts,
+            inFile: input,
+            outFile: output,
+        })
+    })
 
-function help() {
-    console.log(`options:
-    --in <path> required
-    --out <path>
-    --font <path>
-    --template <path>
-    --author <name>
-    -h, --help`)
-}
-
-for (let i = 2; i < process.argv.length; i++) {
-    switch (process.argv[i]) {
-        case '--in':
-            inFile = process.argv[i + 1]
-            break
-        case '--out':
-            outFile = process.argv[i + 1]
-            break
-        case '--font':
-            font = process.argv[i + 1]
-            break
-        case '--template':
-            template = prcess.argv[i + 1]
-            break
-        case '--author':
-            author = process.argv[i + 1]
-            break
-        case '--help':
-        case '-h':
-            help()
-            break
-    }
-}
-
-if (!inFile) {
-    console.log('you must give an file with --in')
-    process.exit(1)
-}
-
-if (!outFile) {
-    outFile = path.join('og', inFile).replace(/.md$/, '.png')
-}
-
-getOgFromMd({ inFile, outFile, font, template, author })
+program.parse()
